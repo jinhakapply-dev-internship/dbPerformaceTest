6f7fb35fd233b96d5bc9a220a7bed0921ec3c5db
HAYEONryu <44168356+HAYEONryu@users.noreply.github.com>
2021-12-31 16:52:39 +0900

    Feature/cont (#11)
    * fix: cors not fixed
    
    * error: db object make error
    
    Co-authored-by: Hayan Jang <61132526+white-jang@users.noreply.github.com>

--- src/main/java/com/example/demo/controller/MainContoller.java
+++ src/main/java/com/example/demo/controller/MainContoller.java
@@ -1,33 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.example.demo.db.DBAdaptor;
-import com.example.demo.dto.DTO;
 
 
 @Controller
 public class MainContoller {
-	
-	private DBAdaptor DB;
-	
 	@RequestMapping(value="/")
 	public String index() {
-		return "index";
-	}
-	
-	@RequestMapping(value="/click")
-	public DTO click() {
-		double result[] = new double[3];
-		int execution_count = 10000;
-		result = DB.getProcedureExecutionStatistics(execution_count);
-		DTO minmaxavg = new DTO();
-		minmaxavg.setMin(result[0]);
-		minmaxavg.setMax(result[1]);
-		minmaxavg.setAvg(result[2]);
-		System.out.println("minmaxavg:" + minmaxavg);		
-		return minmaxavg;
+		return "index.html";
 	}
 
 }
