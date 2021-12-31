7b90f3ffe335e33769172907c90c8bcab7d1c249
Hayan Jang <61132526+white-jang@users.noreply.github.com>
2021-12-31 09:24:46 +0900

    Feature/eventHandler (#2)
    * feat : 결과 확인 div 추가, CSS 수정 작업
    
    * chore : Loading animation file 추가
    
    * feat : Loading 컴포넌트 추가
    
    * feat : Button eventHandler/Loading 처리

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,36 @@
+'use-strict'
+
+window.onload = function () {
+    let max, min, avg = 0;
+    let isApiReturned = false;
+
+    let before = document.querySelector('.before-result-container');
+    let after = document.querySelector('.after-result-container');
+    let loading = document.querySelector('.loading-container');
+    let excuteBtn = document.querySelector('.excute-btn');
+    let reExcuteBtn = document.querySelector('.re-excute-btn');
+
+
+    const showLoading = (isApiReturned, delay) => {
+        before.style.display = "none";
+        after.style.display = "none";
+        loading.style.display = "block";
+
+        setTimeout(() => {
+            if (isApiReturned) {
+                after.style.display = "block";
+                loading.style.display = "none";
+            }
+        }, delay)
+    }
+
+    excuteBtn.addEventListener("click", () => {
+
+        isApiReturned = true;
+        showLoading(isApiReturned, 5000);
+
+        // loading.style.display = "none";
+        // after.style.display = "block";
+
+    })
+}
