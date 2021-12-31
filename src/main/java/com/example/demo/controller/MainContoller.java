2ebe6203473e5eff58de1aedaae3e2dbb132ab63
HAYEONryu <21700470@handong.edu>
2021-12-31 14:35:05 +0900

    feat: controller add model

--- src/main/java/com/example/demo/controller/MainContoller.java
+++ src/main/java/com/example/demo/controller/MainContoller.java
@@ -3,10 +3,16 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.db.DBAdaptor;
+
 @Controller
 public class MainContoller {
+	private DBAdaptor DB;
 	@RequestMapping(value="/")
 	public String index() {
+		double result[] = new double[3];
+		int execution_count = 10000;
+		result = DB.getProcedureExecutionStatistics(execution_count);
 		//System.out.println("hayeon");		
 		return "index";
 	}
