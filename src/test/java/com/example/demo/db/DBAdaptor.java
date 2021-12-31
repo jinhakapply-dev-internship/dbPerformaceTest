f94c830e67c5c789fcaf83e31524fff4ca8f52da
S-J-Kim <hedro0458@gmail.com>
2021-12-31 09:46:28 +0900

    feat: db adaptor 스켈레톤 코드

--- src/test/java/com/example/demo/db/DBAdaptor.java
+++ src/test/java/com/example/demo/db/DBAdaptor.java
@@ -10,17 +10,43 @@ public class DBAdaptor {
 	private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=AdventureWorks2019";
 	private static final String USER = "root";
 	private static final String PW = "1234";
-	
-	@Test
-	public void connectionTest() throws Exception{
+
+	DBAdaptor() throws Exception {
 		Class.forName(Driver);
-		
-		try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+
+		try (Connection con = DriverManager.getConnection(URL, USER, PW)) {
 			System.out.println("Success");
 		} catch (Exception e) {
 			System.out.println(e);
 			e.printStackTrace();
 		}
+
+	}
+
+	private void executeProcedure() throws Exception {
+
+	}
+
+
+	private double getProcedureExecutionTime() throws Exception {
+		double execute_time = 0.0;
+
+		return execute_time;
+
+	}
+
+	public double[] getProcedureExecutionStatstics(int execution_time) {
+		double min = 0.0;
+		double max = 0.0;
+		double avg = 0.0;
+
+		double execution_info[] = new double[3];
+
+		execution_info[0] = min;
+		execution_info[1] = avg;
+		execution_info[2] = max;
+
+		return execution_info;
 	}
 
 }
